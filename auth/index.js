9b2f6413b7096cf53c320531e226bc6e6024df22
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-02-13 21:33:51 -0500

    first commit

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,23 @@
 //This is where we will add our auth strategies
+import { Strategy, ExtractJwt } from "passport";
+
+export default function setUpJWTStrategy(passport){
+    passport.use(
+        new Strategy(
+            {
+                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                secretOrKey: "SECRET KEY" //thisIsASecretKey,
+            },
+            function (payload, done) {
+                try {
+                    return done(null, { username: payload.username, id: payload.id });
+                } catch(e){
+                    return done(e, null);
+                }
+            }
+        )
+    )
+            
+}
+
+
